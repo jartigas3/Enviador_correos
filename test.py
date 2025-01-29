f9d5ca70adee47f105deb89ea3f0a1e7b2d666bf
jartigas3 <jpartigas@miuandes.cl>
2025-01-29 16:31:18 -0300

    gitignore añadido

--- test.py
+++
@@ -1,8 +0,0 @@
-import datetime
-
-
-hoy = datetime.datetime.now()   
-año = hoy.year
-mes = hoy.month
-
-print(f"http://161.131.215.59:8090/wsRentabilidadJs/descargar?ano={año}&mes={mes}&grupo=1&rut=0&sesion=1") 
